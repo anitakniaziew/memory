7cbefac5534775e34db51925addcb2395855297b
anitakniaziew <anita.kniaziew@gmail.com>
2020-01-09 08:35:43 +0100

    simplified checkIfWin function

--- memory.js
+++ memory.js
@@ -8,7 +8,7 @@ window.onload = function() {
   createBacks();
   timer.innerHTML += " 00:00";
 };
-const usedColors = [];
+
 let color1 = null;
 let color2 = null;
 let isField1Clicked = false;
@@ -37,7 +37,7 @@ let colors = [
 function createFronts() {
   for (let i = 0; i < boardLength; i++) {
     board.children[i].onclick = showColor;
-    front = document.createElement("div");
+    const front = document.createElement("div");
     front.classList.add("front");
     front.id = "front" + i;
     board.children[i].appendChild(front);
@@ -46,7 +46,7 @@ function createFronts() {
 
 function createBacks() {
   for (let i = 0; i < boardLength; i++) {
-    back = document.createElement("div");
+    const back = document.createElement("div");
     back.classList.add("back");
     back.classList.add(pickColor());
     back.id = "back" + i;
@@ -61,21 +61,20 @@ function pickColor() {
   return color;
 }
 
-function showColor() {
-  if ((!isField1Clicked, !isField2Clicked)) {
-    const clickedField = event.target;
-    const clickedFieldColor = clickedField.classList[1];
-    clickedField.style = "background-color: " + clickedFieldColor;
-    if (color1) {
-      color2 = clickedFieldColor;
-      field2 = clickedField;
-      isField2Clicked = true;
-      compareColor(color1, color2);
-    } else {
-      color1 = clickedFieldColor;
-      field1 = clickedField;
-      isField1Clicked = true;
-    }
+function showColor(event) {
+  if (isField1Clicked && isField2Clicked) return;
+  const clickedField = event.target;
+  const clickedFieldColor = clickedField.classList[1];
+  clickedField.style = "background-color: " + clickedFieldColor;
+  if (color1) {
+    color2 = clickedFieldColor;
+    field2 = clickedField;
+    isField2Clicked = true;
+    compareColor(color1, color2);
+  } else {
+    color1 = clickedFieldColor;
+    field1 = clickedField;
+    isField1Clicked = true;
   }
 }
 
@@ -102,14 +101,11 @@ function hideColor() {
 
 function checkIfWin() {
   count = 0;
-  for (let i = 0; i < boardLength; i++) {
-    if (
-      board.children[i].lastElementChild.style.backgroundColor !==
-        "transparent" &&
-      board.children[i].lastElementChild.style.backgroundColor !== ""
-    )
-      count += 1;
-  }
+  const fields = Array.from(board.children);
+  fields.forEach(child => {
+    let backColor = child.lastElementChild.style.backgroundColor;
+    if (backColor !== "transparent" && backColor !== "") count += 1;
+  });
   if (count === 16) setTimeout(endGame, 500);
 }
 
